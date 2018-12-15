c52585c982111fe31e6126c53492bc84dd8bf492
Cesar Correchel <cesarcdowns@Cesars-MacBook-Pro.local>
2018-12-15 14:20:14 -0600

    add functions.php and Room custom post type

---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Gutenberg's Hotel functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package gutenbergs-hotel
+ * @since 0.1
+ */
+
+ include_once ('admin/custom-post-types/cpt-rooms.php');
