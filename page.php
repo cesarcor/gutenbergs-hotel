bdeecb2d2e95e709bb0ba53165075115642c8ae9
Cesar Correchel <cesarcdowns@Cesars-MacBook-Pro.local>
2018-12-15 14:14:29 -0600

    add page template

---
+++ page.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package gutenbergs-hotel
+ * @since 0.1
+ */
+
+get_header();
+?>
+
+
+<?php get_footer(); ?>
