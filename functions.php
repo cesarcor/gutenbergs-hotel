788b131454e4432d5554367a9512269afc46a011
Cesar Correchel <cesarcdowns@Cesars-MacBook-Pro.local>
2018-12-20 23:44:55 -0600

    add theme specific block category

--- functions.php
+++ functions.php
@@ -8,4 +8,18 @@
  * @since 0.1
  */
 
+ add_filter( 'block_categories', function( $categories, $post ) {
+ 	return array_merge(
+ 		$categories,
+ 		array(
+ 			array(
+ 				'slug' => 'gh-blocks',
+ 				'title' => __( 'Gutenberg\'s Hotel', 'gh-blocks' ),
+ 			),
+ 		)
+ 	);
+ }, 10, 2 );
+
+ include_once ('blocks/hero-block/hero-block.php');
+
  include_once ('admin/custom-post-types/cpt-rooms.php');
