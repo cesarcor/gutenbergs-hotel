51f97c69ed00a9a295317e2c07170c8b15231f22
Cesar Correchel <cesarcdowns@Cesars-MacBook-Pro.local>
2018-12-20 23:45:45 -0600

    add loop to page template

--- page.php
+++ page.php
@@ -11,5 +11,19 @@
 get_header();
 ?>
 
+<main>
+
+  <?php
+  if ( have_posts() ):
+    while ( have_posts() ): the_post();
+
+    the_content();
+
+    endwhile;
+  endif;
+  ?>
+
+</main>
+
 
 <?php get_footer(); ?>
