c4a1d315a93169e7affa343bc1d306e1a12c1313
Cesar Correchel <cesarcorrechel@gmail.com>
2018-12-27 23:30:09 -0600

    add content templates

---
+++ template-parts/content/content-page.php
@@ -0,0 +1,13 @@
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+  <?php
+  if ( have_posts() ):
+    while ( have_posts() ): the_post();
+
+    the_content();
+
+    endwhile;
+  endif;
+  ?>
+
+</article>
