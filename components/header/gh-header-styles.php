4fe486860e22368e70e92cd3f46fe30390104332
Cesar Correchel <cesarcorrechel@gmail.com>
2019-02-21 15:52:31 -0600

    tweak header style when theme activated

--- components/header/gh-header-styles.php
+++ components/header/gh-header-styles.php
@@ -17,11 +17,42 @@ function gh_custom_header_styles()
       font-size: <?php echo get_theme_mod('gh_set_top_bar_text_size') . 'px'; ?>;
     }
 
-    .main-nav a,
-    .nav-panel a,
-    #left-nav a{
-      color: <?php echo get_theme_mod('gh_set_header_text_color'); ?>
-    }
+    <?php if(get_theme_mod('gh_set_header_text_color')): ?>
+
+      .main-nav a,
+      .nav-panel a,
+      #left-nav a{
+        color: <?php echo get_theme_mod('gh_set_header_text_color'); ?>
+      }
+
+      .hamburger.is-active .hamburger-inner,
+      .hamburger.is-active .hamburger-inner:after,
+      .hamburger.is-active .hamburger-inner:before,
+      .hamburger-inner,
+      .hamburger-inner::before,
+      .hamburger-inner::after{
+        background-color: <?php echo get_theme_mod('gh_set_header_text_color'); ?>
+      }
+
+    <?php else: ?>
+
+      .main-nav a,
+      .nav-panel a,
+      #left-nav a{
+        color: #C8553D;
+      }
+
+      .hamburger.is-active .hamburger-inner,
+      .hamburger.is-active .hamburger-inner:after,
+      .hamburger.is-active .hamburger-inner:before,
+      .hamburger-inner,
+      .hamburger-inner::before,
+      .hamburger-inner::after{
+        background-color: #C8553D;
+      }
+
+    <?php endif; ?>
+
 
     .site-header,
     .nav-panel{
@@ -32,15 +63,8 @@ function gh_custom_header_styles()
       font-size: <?php echo get_theme_mod('gh_set_header_font_size') . 'px'; ?>
     }
 
-    .hamburger.is-active .hamburger-inner,
-    .hamburger.is-active .hamburger-inner:after,
-    .hamburger.is-active .hamburger-inner:before,
-    .hamburger-inner,
-    .hamburger-inner::before,
-    .hamburger-inner::after{
-      background-color: <?php echo get_theme_mod('gh_set_header_text_color'); ?>
-    }
 
+    /* --- SCROLL SETTING --- */
     <?php if (get_theme_mod('gh_set_header_scroll') == 1 &&
              get_theme_mod('gh_set_top_bar') == 1): ?>
 
