89c3b2d6ba2e132cb06ce5813f677626798646ba
Cesar Correchel <cesarcorrechel@gmail.com>
2019-03-04 23:22:06 -0600

    add settings class for sidebar options in customizer

---
+++ inc/customizer/settings/sidebar-settings.php
@@ -0,0 +1,27 @@
+<?php
+
+if(!defined('ABSPATH')) exit;
+
+class GH_Sidebar_Settings{
+
+  public function __construct()
+  {
+      add_action('customize_register', array($this, 'gh_customizer_options'));
+  }
+
+  public function gh_customizer_options($wp_customize)
+  {
+
+    /* ========
+     * Sidebar Panel
+     ========== */
+        $wp_customize->add_panel('gh_panel_sidebar', array(
+                  'title' => __('Layout', 'gh'),
+                  'priority' => 13
+    ));
+
+  }
+
+}
+
+return new GH_Sidebar_Settings();
