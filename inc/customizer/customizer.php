03e8060bd7b9fea2a5ac2e621a10a5e071f046f8
Cesar Correchel <cesarcorrechel@gmail.com>
2019-03-04 23:29:21 -0600

    incorporate new settings inside main customizer file

--- inc/customizer/customizer.php
+++ inc/customizer/customizer.php
@@ -5,4 +5,6 @@ if (! defined('ABSPATH')) exit;
 $settings_dir = GH_THEME_DIR . '/inc/customizer/settings/';
 
 require_once($settings_dir . 'header-settings.php');
+require_once($settings_dir . 'layout-settings.php');
+require_once($settings_dir . 'sidebar-settings.php');
 require_once($settings_dir . 'footer-settings.php');
