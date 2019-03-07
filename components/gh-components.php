87b9366135b6b4147241bcf32770e10cb9528abc
Cesar Correchel <cesarcorrechel@gmail.com>
2019-03-06 23:54:09 -0600

    include footer comp file in main comp file

--- components/gh-components.php
+++ components/gh-components.php
@@ -7,5 +7,6 @@
 
 require_once(GH_THEME_DIR . '/components/header/gh-header-comp.php');
 require_once(GH_THEME_DIR . '/components/header/gh-header-styles.php');
-
 require_once(GH_THEME_DIR . '/components/header/Menu_Walker.php');
+
+require_once(GH_THEME_DIR . '/components/footer/gh-footer-comp.php');
