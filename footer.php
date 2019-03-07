61d31cade93e25567a0e1c7b0b815d5913749228
Cesar Correchel <cesarcorrechel@gmail.com>
2019-03-06 23:55:01 -0600

    include footer comp function

--- footer.php
+++ footer.php
@@ -1,4 +1,18 @@
 
+  <footer id="footer">
+
+    <div class="footer-content-contain">
+      <?php gh_footer_type( get_theme_mod( 'gh_set_footer_type' ) ); ?>
+    </div>
+
+    <div id="copyright">
+
+    </div>
+
+
+  </footer>
+
+
 <?php wp_footer(); ?>
 </body>
 </html>
