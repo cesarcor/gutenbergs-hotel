333d2ba73816775f86c965f4c47810bf34be0a6f
Cesar Correchel <cesarcorrechel@gmail.com>
2019-03-09 23:50:26 -0600

    add layout setting

--- inc/customizer/settings/layout-settings.php
+++ inc/customizer/settings/layout-settings.php
@@ -1,27 +1,50 @@
 <?php
 
-if(!defined('ABSPATH')) exit;
+if (!defined('ABSPATH')) {
+    exit;
+}
 
-class GH_Layout_Settings{
+class GH_Layout_Settings
+{
+    public function __construct()
+    {
+        add_action('customize_register', array($this, 'gh_customizer_options'));
+    }
 
-  public function __construct()
-  {
-      add_action('customize_register', array($this, 'gh_customizer_options'));
-  }
-
-  public function gh_customizer_options($wp_customize)
-  {
+    public function gh_customizer_options($wp_customize)
+    {
 
     /* ========
      * Layout Panel
      ========== */
-        $wp_customize->add_panel('gh_panel_layout', array(
-                  'title' => __('Layout', 'gh'),
-                  'priority' => 12
-    ));
+        //     $wp_customize->add_panel('gh_panel_layout', array(
+        //               'title' => __('Layout', 'gh'),
+        //               'priority' => 12
+        // ));
 
-  }
+        /* ========
+         * Layout Type Section
+         ========== */
+        $wp_customize->add_section('gh_layout_section', array(
+          'title' => __('Layout', 'gh'),
+          'priority' => 12
+        ));
 
+        $wp_customize->add_setting('gh_set_layout_type', array(
+          'default' => 'layout-default'
+        ));
+
+        $wp_customize->add_control('gh_cont_layout_type', array(
+              'label' => __('Page Layout', 'gh'),
+              'section' => 'gh_layout_section',
+              'settings' => 'gh_set_layout_type',
+              'type' => 'select',
+              'choices' => array(
+                      'default' => 'Default with Sidebar',
+                      'full-width' => 'Full Width'
+              )
+            ));
+    }
 }
 
 return new GH_Layout_Settings();
