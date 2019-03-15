11dc1787ec2dc87e449589605a30b8383ae9c2ce
Cesar Correchel <cesarcorrechel@gmail.com>
2019-03-14 21:57:19 -0600

    add file to manage footer customizations

--- components/gh-components.php
+++ components/gh-components.php
@@ -10,3 +10,4 @@ require_once(GH_THEME_DIR . '/components/header/gh-header-styles.php');
 require_once(GH_THEME_DIR . '/components/header/Menu_Walker.php');
 
 require_once(GH_THEME_DIR . '/components/footer/gh-footer-comp.php');
+require_once(GH_THEME_DIR . '/components/footer/gh-footer-styles.php');
