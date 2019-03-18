f362a522025af3280aea6976cb9677028d4cd496
Cesar Correchel <cesarcorrechel@gmail.com>
2019-03-17 23:29:54 -0600

    add single post settings

---
+++ inc/customizer/settings/single-settings.php
@@ -0,0 +1,28 @@
+<?php
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+class GH_Single_Settings
+{
+    public function __construct()
+    {
+        add_action('customize_register', array($this, 'gh_customizer_options'));
+    }
+
+    public function gh_customizer_options($wp_customize)
+    {
+
+    /* ========
+     * Layout Panel
+     ========== */
+            $wp_customize->add_panel('gh_panel_single', array(
+                      'title' => __('Single Post Settings', 'gh'),
+                      'priority' => 12
+        ));
+
+    }
+}
+
+return new GH_Single_Settings();
