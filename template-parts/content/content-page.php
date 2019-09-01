0b0977b9e6e3ab238dd8b8496bb2256af1a1a0cf
Cesar Correchel <cesarcorrechel@gmail.com>
2019-09-01 16:27:07 -0600

    refactor content page template

--- template-parts/content/content-page.php
+++ template-parts/content/content-page.php
@@ -1,13 +1,33 @@
-<article id="page-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-  <?php
+<?php
   if ( have_posts() ):
     while ( have_posts() ): the_post();
+  ?>
 
-    the_content();
+  <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+    <header class="single-entry-header">
+
+      <div class="post-intro">
+        <?php the_title( '<h1 class="single-entry-title">', '</h1>' ); ?>
+      </div>
+
+      <figure>
+        <?php the_post_thumbnail(); ?>
+      </figure>
+
+    </header>
+
+    <div class="single-entry-content">
+
+      <?php the_content(); ?>
+
+    </div>
+
+  </article>
+
+    <?php
 
     endwhile;
   endif;
   ?>
-
-</article>
