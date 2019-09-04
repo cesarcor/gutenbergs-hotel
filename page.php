1b7c23401c11c3a0b4d16497f6a1a7dde3910511
Cesar Correchel <cesarcorrechel@gmail.com>
2019-09-03 23:21:08 -0600

    add conditions for fron-page

--- page.php
+++ page.php
@@ -5,7 +5,7 @@
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy
  *
  * @package gutenbergs-hotel
- * @since 0.1
+ * @since 1.0.0
  */
 
 get_header();
@@ -13,17 +13,25 @@ get_header();
 
 <main id="main" class="site-main">
 
-  <div class="wrapper wrapper-default">
+  <div class="wrapper <?php echo is_front_page() ? 'full-width-wrap' : 'wrapper-default'; ?>">
 
     <div class="wrapper-content">
 
       <?php
-        get_template_part('template-parts/content/content', 'page');
-      ?>
-
+        if(is_front_page()):
+          get_template_part('template-parts/content/content');
+        else:
+          get_template_part('template-parts/content/content', 'page');
+        endif;
+        ?>
       </div>
 
-  <?php get_sidebar(); ?>
+      <?php 
+        /**
+         * Hide sidebar if it's the front page
+         */
+        if(!is_front_page()) get_sidebar(); 
+      ?>
 
   <div>
 
